8fd0f557285a623a5c71499c139546a3ad04ba0d
Sergiogd112 <sergio.gomezdamas@gmail.com>
2021-04-29 11:58:18 +0200

    update

---
+++ Graph/Node.py
@@ -0,0 +1,6 @@
+class Node:
+    def __init__(self,name,segments,nodes):
+        self.name = name
+        self.segments=segments
+        self.nodes = nodes
+        
---
+++ Graph/__init__.py
@@ -0,0 +1,5 @@
+from Node import Node
+from Segment import Segment
+class Graph:
+    def __init__(self,):
+        pass
