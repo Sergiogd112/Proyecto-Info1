cade7171c009e24e60aecd837a319d8ac347d4e4
Alvaro-cast <83400508+Alvaro-cast@users.noreply.github.com>
2021-04-30 09:11:14 +0200

    Update Node.py

--- Graph/Node.py
+++ Graph/Node.py
@@ -1,5 +1,28 @@
+import math
+
+#definition of the class that will contain all the nodes information
 class Node:
-    def __init__(self, name, segments, neightbours,x,y):
-        self.name = name
-        self.segments = segments
-        self.neightbours = neightbours
+    def __init__(self):
+        self.Name = ""
+        self.Xcoordinate = 0.0
+        self.Ycoordinate = 0.0
+        self.Neighbors = []
+
+#definition of the function that will add nodes as neighbours in case they aren't already
+def addNeighbor(n,nd):
+    i = 0
+    found = False
+    while i < len(n.Neighbors) and not found:
+        if n.Neighbors[i] == nd:
+            found = True
+        i += 1
+    if not found:
+        n.Neighbors.append(nd)
+        return True
+    else:
+        return False
+
+#function that will compute the euclidean distance from one node to another 
+def distance(n1, n2):
+    dist = math.sqrt((n2.Xcoordinate - n1.Xcoordinate)**2 + (n2.Ycoordinate - n1.Ycoordinate)**2)
+    return dist
