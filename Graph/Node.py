4ade201a9ed85a9028d83f42feb972ff4ecb55a0
Sergiogd112 <sergio.gomezdamas@gmail.com>
2021-05-02 12:47:23 +0200

    adding docstring to the code

--- Graph/Node.py
+++ Graph/Node.py
@@ -3,7 +3,15 @@ import math
 # definition of the class that will contain all the nodes information
 class Node:
     def __init__(self, Name='', Segments=[], Xcoordinate=0.0, Ycoordinate=0.0, Neighbors=[]):
-        self.Name = Name
+        """Constructor of the Node class: generates a node instance
+
+        Args:
+            Name (str, optional): Name of the Node. Defaults to ''.
+            Segments (list, optional): list of segments conected to the node. Defaults to [].
+            Xcoordinate (float, optional): X coordinate. Defaults to 0.0.
+            Ycoordinate (float, optional): Y coordinate. Defaults to 0.0.
+            Neighbors (list, optional): List of nodes connected. Defaults to [].
+        """        self.Name = Name
         self.Segments = Segments
         self.Xcoordinate = Xcoordinate
         self.Ycoordinate = Ycoordinate
@@ -11,6 +19,15 @@ class Node:
 
     # definition of the function that will add nodes as neighbours in case they aren't already
     def addNeighbor(n, nd):
+        """adds Node to the list of Neightbors
+
+        Args:
+            n (int): position in the list of Neightbors
+            nd (Node): Node to add
+
+        Returns:
+            bool: True if the operation was successful
+        """        
         i = 0
         found = False
         while i < len(n.Neighbors) and not found:
@@ -24,4 +41,3 @@ class Node:
             return False
     def __repr__(self):
         return self.Name+f' ({self.Xcoordinate},{self.Ycoordinate})'
-    # function that will compute the euclidean distance from one node to another
--- Graph/Segment.py
+++ Graph/Segment.py
@@ -5,6 +5,15 @@ class Segment:
     def __init__(
         self, cost=None, origin=None, destination=None, orgcoords=None, destcoords=None
     ):
+        """Constuctor for the segment class
+
+        Args:
+            cost (float, optional): Cost to travers the segment. Defaults to None.
+            origin (str, optional): Name of the node of origin. Defaults to None.
+            destination (str, optional): name of the node of destiny. Defaults to None.
+            orgcoords (float tuple, optional): coordinates of the node of origin. Defaults to None.
+            destcoords (float tuple, optional): coordinates of the node of destination. Defaults to None.
+        """    
         if cost != None and cost != "None" and cost != "unk":
             self.cost = cost
         else:
@@ -13,6 +22,17 @@ class Segment:
         self.destination = destination
 
     def distance(n1=None, n2=None, orgcoords=None, destcoords=None):
+        """Calculates the distance between two nodes or coordinates in the cartesian space
+
+        Args:
+            n1 (Node, optional): Node of origin. Defaults to None.
+            n2 (Node, optional): Node of destination. Defaults to None.
+            orgcoords (float tuple, optional): coordinates of origin. Defaults to None.
+            destcoords (float tuple, optional): coordinates of destination. Defaults to None.
+
+        Returns:
+            float: distance between them in the cartesian space
+        """        
         if n1:
             dist = math.sqrt(
                 (n2.Xcoordinate - n1.Xcoordinate) ** 2
